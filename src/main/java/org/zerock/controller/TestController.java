6386cf2f14ec20a9af11c561cf09efed6dcb7570
fivingo <fiving@naver.com>
2022-01-11 19:42:36 +0900

    22.01.11

---
+++ src/main/java/org/zerock/controller/TestController.java
@@ -0,0 +1,16 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("data", "Hello, Spring from IntelliJ!");
+
+        return "index";
+    }
+}
